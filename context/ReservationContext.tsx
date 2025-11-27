8e5d10d20c180e70805210e0d146c926331ab394
szymonguzik <szymon.guzik@gmail.com>
2025-11-28 00:15:15 +0100

    fix: naprawiono resetReservation - użycie getInitialReservationState()

--- context/ReservationContext.tsx
+++ context/ReservationContext.tsx
@@ -125,7 +125,7 @@ export function ReservationProvider({ children }: ReservationProviderProps) {
   }, []);
 
   const resetReservation = useCallback(() => {
-    setReservation(initialReservationState);
+    setReservation(getInitialReservationState());
   }, []);
 
   const value: ReservationContextType = {
